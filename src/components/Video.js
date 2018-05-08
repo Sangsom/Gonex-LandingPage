68f944deaa11b22e52fe45fb3d740d2ced53d6b1
Rinalds Pudulis <rpudulis@gmail.com>
2018-05-08 14:33:46 +0300

    feat: Added video

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const VideoWrapper = styled.section`
+  position: relative;
+  overflow: hidden;
   display: flex;
   height: 670px;
   background: #324651;
   justify-content: center;
   align-items: center;
+
+  video {
+    width: 100%;
+  }
 `;
 
 const VideoContent = styled.section`
+  position: absolute;
   text-align: center;
 
   h2 {
@@ -35,6 +42,9 @@ class Video extends Component {
   render() {
     return (
       <VideoWrapper>
+        <video autoPlay loop>
+          <source src="../video/video.mov" type="video/mp4" />
+        </video>
         <VideoContent>
           <img src="./img/logo_video.png" alt="" />
           <h2>Show me the video</h2>
